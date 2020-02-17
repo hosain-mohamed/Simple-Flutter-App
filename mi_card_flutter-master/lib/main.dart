25daa29ad1d846509c5f62603482983aada8c4fc
shs442000 <shs442000@gmail.com>
2020-02-17 03:15:10 +0200

    first commit

---
+++ mi_card_flutter-master/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 20,
+                  backgroundImage: AssetImage('images/myphoto.jpg'),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  "Hosain Mohamed",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
